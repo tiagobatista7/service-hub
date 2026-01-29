d9f9010a018f04de47a7f8c192aa1caf90bd7f6e
Tiago Batista <7.tiagobatista@gmail.com>
2026-01-29 12:26:25 -0300

    Revert "Implementado criação de tickets em projetos"
    This reverts commit f72f99830d0cf2efc60c8f130eb6b11a2e496856.

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\TicketController;
-
-Route::get('/projects/{project}/tickets/create', [TicketController::class, 'create'])
-    ->name('tickets.create');
-
-Route::post('/projects/{project}/tickets', [TicketController::class, 'store'])
-    ->name('tickets.store');
+Route::get('/', function () {
+    return view('dashboard');
+});
