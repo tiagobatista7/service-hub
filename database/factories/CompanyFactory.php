6965ebf7a5ebe5878cff4a1165b68fc978beeb60
Tiago Batista <7.tiagobatista@gmail.com>
2026-01-31 02:42:11 -0300

    Ajustado CompanyFactory

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -14,17 +14,7 @@ class CompanyFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->randomElement([
-                'Conecta Sistemas',
-                'Gestão Pro',
-                'Brasil Tech',
-                'Cloud ERP',
-                'Integra Solutions',
-                'Smart Sistemas',
-            ]) . ' ' . $this->faker->randomElement([
-                'LTDA',
-                'S/A',
-            ]),
+            'name' => $this->faker->unique()->company() . ' ' . $this->faker->randomElement(['LTDA', 'S/A']),
         ];
     }
 }
