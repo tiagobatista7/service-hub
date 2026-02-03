b88d282c5307bb57b37a2ef626febcc2260d4a01
Tiago Batista <7.tiagobatista@gmail.com>
2026-02-03 00:21:34 -0300

    Ajustado e inclusão do show vue

---
+++ resources/js/Pages/Projects/Show.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ project.name }}</h1>
+
+    <h2>Tickets</h2>
+
+    <ul>
+      <li v-for="ticket in project.tickets" :key="ticket.id">
+        {{ ticket.title }} - {{ ticket.status }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  project: Object
+})
+</script>
